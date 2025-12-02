12077def56a9a15dd8fb9492ea7cb8df18f7cde3
Wilson Bastidas <wilson.bastidas@pragma.com.co>
2025-12-02 16:33:48 -0500

    initial commit

---
+++ src/main/java/com/pragma/ms_small_square/MsSmallSquareApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.ms_small_square;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSmallSquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSmallSquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pragma/ms_small_square/MsSmallSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.ms_small_square;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSmallSquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
